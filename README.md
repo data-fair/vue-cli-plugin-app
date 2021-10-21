e1e9d03a32f93c105149b4264e90a091437dc198
Alban Mouton <alban.mouton@gmail.com>
2021-10-21 14:37:19 +0200

    chore: small doc and env files improvements

--- README.md
+++ README.md
@@ -10,4 +10,21 @@ A vue-cli plugin to create data-fair applications
     vue add vuex
     vue add vuetify # chose the default preset
     vue add @data-fair/app
+    npm run lint
     npm run dev
+
+## Development
+
+Test the plugin from local code source
+
+    npm link
+    cd ..
+    vue create test-app # the default vue 2 preset is ok
+    cd test-app
+    vue add vuex
+    vue add vuetify # chose the default preset
+    npm link @data-fair/vue-cli-plugin-app
+    vue invoke @data-fair/app
+    npm run lint
+    npm run dev
+    
--- generator/index.js
+++ generator/index.js
@@ -1,19 +1,19 @@
 module.exports = (api, options) => {
   api.extendPackage({
     scripts: {
-      dev: 'APP_URL=http://localhost:8080/app df-dev-server',
-      'dev-src': 'vue-cli-service serve',
+      dev: 'APP_URL=http://localhost:8384/app df-dev-server',
+      'dev-src': 'vue-cli-service serve --port 8384',
       prepublish: `PUBLIC_URL=https://cdn.jsdelivr.net/npm/${api.rootOptions.projectName}@\${npm_package_version}/dist vue-cli-service build --modern`,
-      postpublish: "node scripts/postpublish.js"
+      postpublish: 'node scripts/postpublish.js',
     },
     dependencies: {
       '@data-fair/dev-server': '^1.0.1',
-      "@data-fair/components-app": "^0.2.3",
-      axios: '^0.21.1'
+      '@data-fair/components-app': '^0.2.6',
+      axios: '^0.21.1',
     },
     devDependencies: {
       'eslint-config-vuetify': '^0.6.1',
-      semver: '^7.3.5'
+      semver: '^7.3.5',
     },
     eslintConfig: null, // replace by eslintrc.js template
   })
